4c4e3f86922ab2d6c9b576960ab525130a5ed889
RichoDemus <git@richodemus.com>
2020-05-06 12:31:34 +0200

    Fix optional dependencies

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -21,7 +21,9 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen(js_name = getBiomes)]
 pub fn get_biomes(blacklist: Array) -> Result<Map, JsValue> {
+    #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
+
     let blacklist: Vec<Id> = blacklist
         .to_vec()
         .into_iter()
